b71a5ad0e9330506476242869b3d06394c0d3200
M Abdul Aziz <muhammadabdulaziz@gmail.com>
2019-11-13 09:37:04 +0700

    add authentication

--- routes/customers.js
+++ routes/customers.js
@@ -1,22 +1,23 @@
 var express = require('express');
 var router = express.Router();
-
+var authentication_mdl = require('../middlewares/authentication');
+var session_store;
 /* GET Customer page. */
 
-router.get('/', function(req, res, next) {
+router.get('/',authentication_mdl.is_login, function(req, res, next) {
 	req.getConnection(function(err,connection){
 		var query = connection.query('SELECT * FROM customer',function(err,rows)
 		{
 			if(err)
 				var errornya  = ("Error Selecting : %s ",err );   
 			req.flash('msg_error', errornya);   
-			res.render('customer/list',{title:"Customers",data:rows});
+			res.render('customer/list',{title:"Customers",data:rows,session_store:req.session});
 		});
          //console.log(query.sql);
      });
 });
 
-router.delete('/delete/(:id)', function(req, res, next) {
+router.delete('/delete/(:id)',authentication_mdl.is_login, function(req, res, next) {
 	req.getConnection(function(err,connection){
 		var customer = {
 			id: req.params.id,
@@ -39,7 +40,7 @@ router.delete('/delete/(:id)', function(req, res, next) {
 		});
 	});
 });
-router.get('/edit/(:id)', function(req,res,next){
+router.get('/edit/(:id)',authentication_mdl.is_login, function(req,res,next){
 	req.getConnection(function(err,connection){
 		var query = connection.query('SELECT * FROM customer where id='+req.params.id,function(err,rows)
 		{
@@ -58,7 +59,7 @@ router.get('/edit/(:id)', function(req,res,next){
 				else
 				{	
 					console.log(rows);
-					res.render('customer/edit',{title:"Edit ",data:rows[0]});
+					res.render('customer/edit',{title:"Edit ",data:rows[0],session_store:req.session});
 
 				}
 			}
@@ -66,7 +67,7 @@ router.get('/edit/(:id)', function(req,res,next){
 		});
 	});
 });
-router.put('/edit/(:id)', function(req,res,next){
+router.put('/edit/(:id)',authentication_mdl.is_login, function(req,res,next){
 	req.assert('name', 'Please fill the name').notEmpty();
 	var errors = req.validationErrors();
 	if (!errors) {
@@ -105,7 +106,7 @@ router.put('/edit/(:id)', function(req,res,next){
 	}else{
 
 		console.log(errors);
-		errors_detail = "<p>Sory there are error</p><ul>";
+		errors_detail = "Sorry there are error<ul>";
 		for (i in errors) 
 		{ 
 			error = errors[i]; 
@@ -113,15 +114,11 @@ router.put('/edit/(:id)', function(req,res,next){
 		} 
 		errors_detail += "</ul>"; 
 		req.flash('msg_error', errors_detail); 
-		res.render('customer/add-customer', 
-		{ 
-			name: req.param('name'), 
-			address: req.param('address')
-		});
+		res.redirect('/customers/edit/'+req.params.id);
 	}
 });
 
-router.post('/add', function(req, res, next) {
+router.post('/add',authentication_mdl.is_login, function(req, res, next) {
 	req.assert('name', 'Please fill the name').notEmpty();
 	var errors = req.validationErrors();
 	if (!errors) {
@@ -151,6 +148,7 @@ router.post('/add', function(req, res, next) {
 						address: req.param('address'),
 						email: req.param('email'),
 						phone: req.param('phone'),
+						session_store:req.session,
 					});
 				}else{
 					req.flash('msg_info', 'Create customer success'); 
@@ -161,7 +159,7 @@ router.post('/add', function(req, res, next) {
 	}else{
 
 		console.log(errors);
-		errors_detail = "<p>Sory there are error</p><ul>";
+		errors_detail = "Sory there are error<ul>";
 		for (i in errors) 
 		{ 
 			error = errors[i]; 
@@ -172,20 +170,22 @@ router.post('/add', function(req, res, next) {
 		res.render('customer/add-customer', 
 		{ 
 			name: req.param('name'), 
-			address: req.param('address')
+			address: req.param('address'),
+			session_store:req.session
 		});
 	}
 
 });
 
-router.get('/add', function(req, res, next) {
+router.get('/add',authentication_mdl.is_login, function(req, res, next) {
 	res.render(	'customer/add-customer', 
 	{ 
 		title: 'Add New Customer',
 		name: '',
 		email: '',
 		phone:'',
-		address:''
+		address:'',
+		session_store:req.session
 	});
 });
 
